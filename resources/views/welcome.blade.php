cc31bb2f9d4aaa1f8d7e22e374acae49fa4664db
Christian <christian@murphyvarley.com>
2019-01-24 17:31:20 +0000

    Style changes

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -2,7 +2,7 @@
 <div class="homepage__bg">
     <div class="homepage__welcome-area">
         <h3 class="homepage__subtitle">PL Predictor 18/19</h3>
-        <button type="button" class="homepage__btn homepage__btn--inverse btn-block">Register</button>
-        <button type="button" class="homepage__btn btn-block">Log In</button>
+        <a href="register" class="homepage__btn homepage__btn--inverse btn-block">Register</a>
+        <a href="login" class="homepage__btn btn-block">Log In</a>
     </div>
 </div>
